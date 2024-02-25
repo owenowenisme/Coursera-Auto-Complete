e307a69ff044e96150adf83e6df988249b100891
Owen Lin <106612301+owenowenisme@users.noreply.github.com>
2024-02-25 23:46:46 +0800

    Update userid.js

--- userid.js
+++ userid.js
@@ -1,4 +1,4 @@
-const userId = 'Please replace this with your user id from coursera.';
+const userId = //yourid;
 // Get userid: 
 // Enter window.App.context.dispatcher.stores.ApplicationStore.userData.id in your browser console
 // and copy the value of id
