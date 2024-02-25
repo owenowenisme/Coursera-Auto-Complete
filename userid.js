bf43d7a7fe9a8cc964677a3a4bc76113d850af66
Owen Lin <106612301+owenowenisme@users.noreply.github.com>
2024-02-25 23:49:35 +0800

    Update userid.js

--- userid.js
+++ userid.js
@@ -1,4 +1,5 @@
-const userId = //yourid;
+const userId = //yourid save with int, DO NOT save with string.;
+
 // Get userid: 
 // Enter window.App.context.dispatcher.stores.ApplicationStore.userData.id in your browser console
 // and copy the value of id
